1662982153f6d518ff458ee7e32b966caad9c75c
Gasvn7 <96235192+Gasvn7@users.noreply.github.com>
2022-02-15 01:01:11 -0300

    process.env.PORT || 3000

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/lo
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
 
-app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+app.listen(process.env.PORT || 3000, () => console.log("Servidor corriendo en Puerto: 3000"));

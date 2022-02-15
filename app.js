924ed2572786241abe9a001d66d147be23618765
Gasvn7 <96235192+Gasvn7@users.noreply.github.com>
2022-02-15 00:54:33 -0300

    [Lunes15]

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const path = require('path');
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/productDetail.html')));
+
+app.get('/detallesDeProductos', (req, res) => res.sendFile(path.resolve(__dirname, './views/productDetail.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
 
 app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));

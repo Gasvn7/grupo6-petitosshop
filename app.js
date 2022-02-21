be42f81557d3ac140028c88d645e238701c98b9a
Fachifachi <gist140920@gmail.com>
2022-02-20 22:00:51 -0300

    uwu

--- app.js
+++ app.js
@@ -10,5 +10,6 @@ app.get('/detallesDeProductos', (req, res) => res.sendFile(path.resolve(__dirnam
 app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
+app.get('/carrito', (req, res) => res.sendFile(path.resolve(__dirname, './views/carrito.html')));
 
 app.listen(process.env.PORT || 3000, () => console.log("Servidor corriendo en Puerto: 3000"));
